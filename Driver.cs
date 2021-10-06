e884afb43a3eb5efc00bc03709afa99c017e7dc3
aqsanna <52932434+aqsanna@users.noreply.github.com>
2021-10-06 09:56:16 +0400

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualSportSeleniumTests
+{
+
+    public  class Driver
+    {
+      public  IWebDriver driver = new ChromeDriver();
+        
+        //IWebDriver driverOpera = new OperaDriver();
+
+        [SetUp]
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("https://www.totogaming.am/en");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [TearDown]
+        public void Close()
+        {
+           // driver.Close();
+            driver.Quit();
+        }
+
+    }
+
+}
